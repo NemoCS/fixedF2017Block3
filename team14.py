2a0f7b548ff6fb219e88c855bc95b5a7479217ec
braking2017 <32551406+braking2017@users.noreply.github.com>
2017-10-09 13:28:56 -0500

    brandon king team 14

--- team14.py
+++ team14.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'name' # Only 10 chars displayed.
+strategy_name = 'try not to go to prison'
+strategy_description = 'random'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,10 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    import random
     
-    return 'c'
+    C = ['b', 'c']
+    return random.choice(C)
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
