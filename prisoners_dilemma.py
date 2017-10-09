c423b95a02d8f908abd18275093082fc6497b53e
luhuffman <32550916+luhuffman@users.noreply.github.com>
2017-10-09 12:53:02 -0500

    Huffman practice
    Practice

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -44,7 +44,7 @@ collude = example0
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
 team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
 team11, team12, team13, team14]
-teams=[team0,team1,team2,team3,team4,team5,team6,team7,team8,team9]
+teams=[example0, example1, example2, example3, example4, example5, example6, example7,team1]
 for module in modules:
     reload(module)
     print ('reloaded',module)
