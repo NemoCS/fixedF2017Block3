d2d359c82278ff712408943b928da0fa35b0a9f3
upsetstomach56 <32550920+upsetstomach56@users.noreply.github.com>
2017-10-09 12:53:45 -0500

    zebs random stuff

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -44,7 +44,7 @@ collude = example0
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
 team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
 team11, team12, team13, team14]
-teams=[team0,team1,team2,team3,team4,team5,team6,team7,team8,team9]
+teams=[team2,example0, example1, example2, example3, example4, example5, example6, example7]
 for module in modules:
     reload(module)
     print ('reloaded',module)
@@ -247,6 +247,7 @@ def make_section1(modules, scores):
     # Last line
     section1 += 'TOTAL  :'
     for index in range(len(modules)):
+
         section1 += '{:>7}'.format(sum(scores[index]))     
     return section1+'\n'
     
