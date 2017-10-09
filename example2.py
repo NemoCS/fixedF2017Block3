88d1f7ebebb56d85cd4213c1d79472c9e7676d5e
Dan Niemitalo <dniemitalo@linnmar.k12.ia.us>
2017-10-09 13:58:11 -0500

    Nemo change Example2
    Changed example 2 to a different strategy.

--- example2.py
+++ example2.py
@@ -11,18 +11,10 @@ strategy_name = 'Alternate'
 strategy_description = 'Collude, then alternate.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    # This player colludes on even numbered rounds (first round is round #0).
-    if len(my_history)%2 == 0:
+    if len(my_history) == 0:
         return 'c'
-    else:
+    if len(my_history)%4 == 0:
         return 'b'
+    else:
+        return 'c'
     
