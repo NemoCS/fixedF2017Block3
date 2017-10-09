d228a4cc297558336318e0ac90ad44223821fd6a
alyssa1865 <32550906+alyssa1865@users.noreply.github.com>
2017-10-09 12:46:41 -0500

    Jake and Alyssa - Team7
    handshake plan

--- team7.py
+++ team7.py
@@ -6,18 +6,16 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Retal-b' # Only 10 chars displayed.
-strategy_name = 'Greedy Retaliator'
-strategy_description = 'Greedy Retaliator'
+team_name = 'HackSlash' # Only 10 chars displayed.
+strategy_name = 'Handshake'
+strategy_description = 'Handshake'
     
 def move(my_history, their_history, my_score, their_score):
-    #betray on the first turn
-    if len(their_history)==0:
-      return 'b'
-    #betray if opponent successfully betrayed last turn
-    #(this does not include cases when both players betrayed)
-    elif their_history[-1] == 'b' and my_history[-1] == 'c':
-      return 'b'
-    #otherwise collude
-    else:
-      return 'c'
+    if len(their_history) == 0:
+        return 'b'
+    if len(their_history) == 1:
+        return 'c'
+    if their_history == my_history:
+        return 'c' 
+    else: 
+        return 'b'
